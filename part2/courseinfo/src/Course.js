605c6e99d3791bcdb81c2dc0623a0f0433041ac0
jzandag <zidrexandag10@gmail.com>
2021-03-28 16:46:21 +0800

    added ex 2.5, seperated Course component to different module

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -1,5 +1,53 @@
 import React from 'react';
 
-const Course = () => {
-    
-}
+const Header = ({course}) => {
+    return (
+      <h1>{course}</h1>
+    )
+  }
+  
+  const Total = ({ course }) => {
+    const total = course.reduce((x, curr) => {
+      return curr.exercises + x
+    }, 0)
+  
+    return(
+      <p>Total of {total} exercises</p>
+    ) 
+  }
+  
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>    
+    )
+  }
+  
+  const Content = ({ parts }) => {
+    let part = parts.map(el => {
+        return <Part key={el.id}
+          part={el}/>
+    })
+    return (
+      <div>
+        {part}
+      </div>
+    )
+  }
+  
+const Course = ({course}) => {
+    let courses = course.map(el => {
+      return <div key={el.id}>
+          <Header course={el.name}/>
+        <Content parts={el.parts}/>
+        <Total course={el.parts}/>
+        </div>
+    })
+    return (
+      <div>
+        {courses}
+      </div>
+    )
+  }
+export default Course;
